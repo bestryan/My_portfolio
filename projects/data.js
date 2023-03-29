8c0536eee9efe121c265f0c4c287d71a80f073b9
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-03-30 10:20:23 +1100

    script.js update

--- projects/data.js
+++ projects/data.js
@@ -14,7 +14,7 @@ export const projects = [
         tools: "Languages & Tools: Java & IDE",
         description: "The objective of this project is to assess my skills required to perform coding, testing and debugging activities in Java. Task one read any given day's observations, calculates rolling averages temperature, dew point, humidity and wind Speed. Write new record to a csv file. Task two declare an array to perform insertion algorithms, random access algorithms, deletion algorithms, sequential access algorithm and binary search algorithm.",
         code: "https://github.com/bestryan/Java_code_challenges",
-        demo: "#",
+        demo: "",
         photo: "/projects/images/java_code.png"
     },
     {
@@ -23,7 +23,7 @@ export const projects = [
         tools: "Languages & Tools: Java & IDE",
         description: "This simple Mastermind number guess game build with Java. The computer will think of 4 secret numbers. You try to guess what numbers are in the code and what order they are in. After making a guess the result will be displayed. Every time you make a guess you get feedback. If you have guessed the right number in the right place you get a Sun. If you have guessed the right number in the wrong place you get a Moon If you have guessed the wrong number you get no feedback.",
         code: "https://github.com/bestryan/Mastermind",
-        demo: "#",
+        demo: "",
         photo: "/projects/images/mastermind_proj.svg"
     },
     {
@@ -32,7 +32,7 @@ export const projects = [
         tools: "Languages & Tools: HTML, CSS, VS Code",
         description: "The objective of this project is to assess my knowledge and performance to design, create and save basic mark-up language documents and cascading style sheets (CSS) to define and style a website. This is my first webiste proejct.",
         code: "https://github.com/bestryan/Sorrento_project_tafe",
-        demo: "https://bestryan.github.io/Sorrento_project_tafe/",
+        demo: "",
         photo: "/projects/images/web_proj.png"
     }
 ];
