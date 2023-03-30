145c3141b6a7ffbec1dafaf6ddf394f46718264a
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-03-30 16:45:20 +1100

    task-app added

--- projects/data.js
+++ projects/data.js
@@ -2,14 +2,23 @@ export const projects = [
     {
         id: 1,
         title: "Create Modern React Website",
-        tools: "Languages & Tools: React, JavaScript and TailwindCSS",
-        description: "The project involves creating a state-of-the-art, modern banking front-end website utilizing React. The site is fully responsive on desktops, medium-sized screens, and mobile devices. TailwindCSS was used in this project to write more clean CSS or Sass.",
+        tools: "Languages & Tools: React, JavaScript and Tailwind-CSS",
+        description: "The project involves creating a state-of-the-art, modern banking front-end website utilizing React. The site is fully responsive on desktops, medium-sized screens, and mobile devices. TailwindCSS was used in this project to write more clean CSS.",
         code: "https://github.com/bestryan/react_web_project/tree/main/bank_modern_web_app_practice",
         demo: "https://bestryan-reactweb.netlify.app",
         photo: "/projects/images/modern_web.png"
     },
     {
         id: 2,
+        title: "Develop a Personal Task Lists",
+        tools: "Languages & Tools: React, JavaScript & CSS",
+        description: "This small application was developed in React with state & effect hooks. It allows users to add, edit, and delete tasks. Additionally, it allowed the user to mark off completed tasks. Users got a better experience with modern user interfaces and local storage. This is an upgraded version of the previous simple to-do list react app.",
+        code: "https://github.com/bestryan/React_multi_projects/tree/main/task-app",
+        demo: "https://ryan-xiao-task.netlify.app",
+        photo: "/projects/images/task.png"
+    },
+    {
+        id: 3,
         title: "Java code challenge - read weather data, export csv file & arraylist manipulate",
         tools: "Languages & Tools: Java & IDE",
         description: "The objective of this project is to assess my skills required to perform coding, testing and debugging activities in Java. Task one read any given day's observations, calculates rolling averages temperature, dew point, humidity and wind Speed. Write new record to a csv file. Task two declare an array to perform insertion algorithms, random access algorithms, deletion algorithms, sequential access algorithm and binary search algorithm.",
@@ -18,7 +27,7 @@ export const projects = [
         photo: "/projects/images/java_code.png"
     },
     {
-        id: 3,
+        id: 4,
         title: "Mastermind - simple game with Java",
         tools: "Languages & Tools: Java & IDE",
         description: "This simple Mastermind number guess game build with Java. The computer will think of 4 secret numbers. You try to guess what numbers are in the code and what order they are in. After making a guess the result will be displayed. Every time you make a guess you get feedback. If you have guessed the right number in the right place you get a Sun. If you have guessed the right number in the wrong place you get a Moon If you have guessed the wrong number you get no feedback.",
@@ -27,7 +36,7 @@ export const projects = [
         photo: "/projects/images/mastermind_proj.svg"
     },
     {
-        id: 4,
+        id: 5,
         title: "Create and style simple mark-up language",
         tools: "Languages & Tools: HTML, CSS, VS Code",
         description: "The objective of this project is to assess my knowledge and performance to design, create and save basic mark-up language documents and cascading style sheets (CSS) to define and style a website. This is my first webiste proejct.",
