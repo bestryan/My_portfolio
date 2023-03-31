4fb63458160d4b464f3164755ca98a537e9f1ba7
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-03-31 12:29:30 +1100

    project added

--- projects/data.js
+++ projects/data.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         id: 1,
-        title: "Create Modern React Website",
+        title: "Create modern React website",
         tools: "Languages & Tools: React, JavaScript and Tailwind-CSS",
         description: "The project involves creating a state-of-the-art, modern banking front-end website utilizing React. The site is fully responsive on desktops, medium-sized screens, and mobile devices. TailwindCSS was used in this project to write more clean CSS.",
         code: "https://github.com/bestryan/react_web_project/tree/main/bank_modern_web_app_practice",
@@ -10,7 +10,7 @@ export const projects = [
     },
     {
         id: 2,
-        title: "Develop a Personal Task Lists",
+        title: "Develop a personal task list",
         tools: "Languages & Tools: React, JavaScript & CSS",
         description: "This small application was developed in React with state & effect hooks. It allows users to add, edit, and delete tasks. Additionally, it allowed the user to mark off completed tasks. Users got a better experience with modern user interfaces and local storage. This is an upgraded version of the previous simple to-do list react app.",
         code: "https://github.com/bestryan/React_multi_projects/tree/main/task-app",
@@ -19,6 +19,15 @@ export const projects = [
     },
     {
         id: 3,
+        title: "Portfolio website",
+        tools: "Languages & Tools: JavaScript, HTML, CSS",
+        description: "This exciting portfolio website was developed through my font-end learning experience. the purpose is to demonstrate my skills to potential employers and clients. Tech stacks like HTML, CSS, JavaScript and jQuery are in use. Hosted with Netfilty and sourced from GitHub. Instead of hardcoding, the projects section was rendered with JS objects.",
+        code: "https://github.com/bestryan/My_portfolio",
+        demo: "http://ryan-xiao.com",
+        photo: "/projects/images/my_site.png"
+    },
+    {
+        id: 4,
         title: "Java code challenge - read weather data, export csv file & arraylist manipulate",
         tools: "Languages & Tools: Java & IDE",
         description: "The objective of this project is to assess my skills required to perform coding, testing and debugging activities in Java. Task one read any given day's observations, calculates rolling averages temperature, dew point, humidity and wind Speed. Write new record to a csv file. Task two declare an array to perform insertion algorithms, random access algorithms, deletion algorithms, sequential access algorithm and binary search algorithm.",
@@ -27,7 +36,7 @@ export const projects = [
         photo: "/projects/images/java_code.png"
     },
     {
-        id: 4,
+        id: 5,
         title: "Mastermind - simple game with Java",
         tools: "Languages & Tools: Java & IDE",
         description: "This simple Mastermind number guess game build with Java. The computer will think of 4 secret numbers. You try to guess what numbers are in the code and what order they are in. After making a guess the result will be displayed. Every time you make a guess you get feedback. If you have guessed the right number in the right place you get a Sun. If you have guessed the right number in the wrong place you get a Moon If you have guessed the wrong number you get no feedback.",
@@ -36,7 +45,7 @@ export const projects = [
         photo: "/projects/images/mastermind_proj.svg"
     },
     {
-        id: 5,
+        id: 6,
         title: "Create and style simple mark-up language",
         tools: "Languages & Tools: HTML, CSS, VS Code",
         description: "The objective of this project is to assess my knowledge and performance to design, create and save basic mark-up language documents and cascading style sheets (CSS) to define and style a website. This is my first webiste proejct.",
